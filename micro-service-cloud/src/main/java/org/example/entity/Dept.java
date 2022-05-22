08a17d94a70ba516e008ac1b96a388a946457a41
Kersinal <1982903990@qq.com>
2022-05-22 13:20:18 +0800

    test

--- micro-service-cloud/src/main/java/org/example/entity/Dept.java
+++ micro-service-cloud/src/main/java/org/example/entity/Dept.java
@@ -4,6 +4,9 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.experimental.Accessors;
 
+/**
+ * @author KHHM
+ */
 @NoArgsConstructor //无参构造函数
 @Data // 提供类的get、set、equals、hashCode、canEqual、toString 方法
 @Accessors(chain = true) //链式编程
